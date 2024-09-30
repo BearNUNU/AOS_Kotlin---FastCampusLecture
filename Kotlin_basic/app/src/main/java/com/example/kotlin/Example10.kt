39e28803f690ea2312ab73109f16b7b64b2f6f21
GunWoo <kkw31025@gmail.com>
2024-09-30 15:43:46 +0900

    [Day1] - Kotlin String template

---
+++ Kotlin_basic/app/src/main/java/com/example/kotlin/Example10.kt
@@ -0,0 +1,10 @@
+package com.example.kotlin
+
+fun main(){
+    val a = 10
+    val name ="안녕"
+    val isHigh = true
+    println(a.toString() + name +isHigh.toString())
+
+    println("$a $name $isHigh")
+}

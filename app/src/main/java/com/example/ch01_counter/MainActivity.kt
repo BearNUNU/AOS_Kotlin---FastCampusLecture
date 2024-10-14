d79ba68f6b3aa1c8d6f78790dccd09be4ac812f8
BearNUNU <kkw31025@gmail.com>
2024-10-14 23:05:03 +0900

    [Day4] - Ch01.Counter App 프로젝트 세팅

---
+++ app/src/main/java/com/example/ch01_counter/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.ch01_counter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

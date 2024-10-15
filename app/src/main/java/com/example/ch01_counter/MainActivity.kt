f272b831cf4ee388d2b8534012a99a64f6b0ef36
BearNUNU <kkw31025@gmail.com>
2024-10-15 20:36:56 +0900

    [Day5] - MainActivity textCounter, buttonRest, buttonPlus 바운딩 및 동작 추가

--- app/src/main/java/com/example/ch01_counter/MainActivity.kt
+++ app/src/main/java/com/example/ch01_counter/MainActivity.kt
@@ -1,10 +1,25 @@
 package com.example.ch01_counter
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textCount = findViewById<TextView>(R.id.textCount)
+        val buttonReset = findViewById<Button>(R.id.buttonReset)
+        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
+        var count = 0
+        buttonReset.setOnClickListener {
+            count = 0
+            textCount.text = count.toString()
+        }
+        buttonPlus.setOnClickListener {
+            count++
+            textCount.text = count.toString()
+        }
     }
 }

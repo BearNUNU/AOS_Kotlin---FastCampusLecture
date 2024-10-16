ad2478f47436cfb9564bf493e2078958c454ca32
BearNUNU <kkw31025@gmail.com>
2024-10-16 23:48:04 +0900

    [Day6] - activity_main( 변환 페이지) 레이아웃 생성 및 activity에 바인딩

--- Ch02_UnitChanger/app/src/main/java/com/example/ch02_unitchanger/MainActivity.kt
+++ Ch02_UnitChanger/app/src/main/java/com/example/ch02_unitchanger/MainActivity.kt
@@ -1,47 +1,22 @@
 package com.example.ch02_unitchanger
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.ch02_unitchanger.ui.theme.Ch02_UnitChangerTheme
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ch02_unitchanger.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Ch02_UnitChangerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Ch02_UnitChangerTheme {
-        Greeting("Android")
+       binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val outputTextView = binding.textOutput
+        val outputUnitTextView = binding.textOutputUnit
+        val inputEditText = binding.textInput
+        val inputUnitTextView = binding.textInputUnit
+        val buttonConverter = binding.buttonUnitConverter
+        var inputNum = 0
     }
 }
